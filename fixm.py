ffa1c55d794234565876c6bdd240dd7df8c3eb3d
Doyoun <42713767+dkim0615@users.noreply.github.com>
2020-07-30 21:24:46 +0800

    Update fixm.py

--- fixm.py
+++ fixm.py
@@ -42,41 +42,47 @@ def fix_spec():
     fix_description = {
     "35" : {"0" : "Heartbeat", "1" : "Test Request", "2" : "Resend Request", "3" : "Reject", "4" : "Sequence Reset", "5" : "Logout", "8" : "Execution Report", "D" : "Order - Single", "Q" : "DK Trade"},
     "21" : {"1" : "AUTO", "2" : "AUTO/MAN", "3" : "MAN"},
+    "22" : {"1" : "CUSIP", "2" : "SEDOL", "3" : "QUIK", "4" : "ISIN", "5" : "RIC"},
     "40" : {"1" : "Market", "2" : "Limit", "3" : "Stop", "4" : "Stop Limit", "5" : "Market on close"},
     "54" : {"1" : "BUY", "2" : "SELL"},
     "59" : {"0" : "DAY", "1" : "GTC", "2" : "OPG", "3" : "IOC", "4" : "FOK", "6" : "GTD"},
     "150" : {"0" : "NEW", "1" : "Partial fill", "2" : "Fill", "3" : "Done for Day", "4" : "Canceled", "5" : "Replaced", "6" : "Pending Cancel", "8" : "Rejected", "E" : "Pending Replace"}
     }
 
-    return fix_dictionary, fix_description
+    return fix_dictionary, fix_description 
 
-def make_list(fix_tag):
-    list1 = []
-    for item in fix_tag:
-        list1.append(fix_tag)
-        list1 = sorted(list1)
-        return list1
-
-def fix_translation(fixm):
+def make_list(fixm):
     fix_dictionary, fix_description = fix_spec()
-    for tags in fixm:
-        tags = tags.strip()
-        fix_tag = tags[:tags.find("=")]
-        fix_tag = fix_tag.replace(" ","")
-        #add fix tags to an empty list to sort numerically and output them back as type str
-        fix_value = tags[tags.find("="):len(tags)].replace("=", "")
-        if fix_tag == '':
+    empty_list = []
+    values_list = []
+    sorted_list = []
+
+    for item in fixm:
+        if item =='':
             continue
-        elif fix_tag in fix_dictionary and fix_tag not in fix_description:
-            print(fix_tag.rjust(6," ") + ":" + str(fix_dictionary[fix_tag]).rjust(25," ") + " " + "=" + " " + str(fix_value).ljust(40," "))
-        elif fix_tag in fix_description:
-            print(fix_tag.rjust(6," ") + ":" + str(fix_dictionary[fix_tag]).rjust(25," ") + " " + "=" + " " + str(fix_value + "(" + fix_description[fix_tag][fix_value] + ")").ljust(40," "))
+        num = item[:item.find("=")]
+        num = int(num)
+        empty_list.append(num)
+        fix_value = item[item.find("="):len(item)].replace("=","")
+        values_list.append(fix_value)
+
+    zip(empty_list, values_list)
+    sorted_list = list(zip(empty_list, values_list))
+    sorted_list.sort()
+    print("\n")
+    #print(sorted_list)
+    
+    for sorted_item in sorted_list:
+        if str(sorted_item[0]) in fix_dictionary and str(sorted_item[0]) not in fix_description:
+            print(str(sorted_item[0]).rjust(6," ") + ":" + str(fix_dictionary[str(sorted_item[0])]).rjust(20," "),"=", sorted_item[1].ljust(40," "))
+        elif str(sorted_item[0]) in fix_description:
+            print(str(sorted_item[0]).rjust(6," ") + ":" + str(fix_dictionary[str(sorted_item[0])]).rjust(20," "),"=", sorted_item[1] + "(" + fix_description[str(sorted_item[0])][sorted_item[1]] + ")".ljust(40," "))
         else:
-            print(fix_tag.rjust(6," ") + ":" + str(fix_value).rjust(25," ") + " " + "=" + " " + "not found in fix dictionary".ljust(40," "))
+            print(str(sorted_item[0]).rjust(6," ") + ":" + str(sorted_item[0]).rjust(20," "),"=", sorted_item[1].ljust(40," "))
 
 result = fix_spec()
-print(result)
+#print(result)
 
 fix_message = input("Please enter a fix message: ")
 fixm = fix_message.split('|')
-fix_translation(fixm)
+make_list(fixm)
